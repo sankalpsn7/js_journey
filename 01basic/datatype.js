12430ba93addff7b82864777960ca2b10a0ebc2d
Sankalp <146836635+sankalpsn7@users.noreply.github.com>
2025-02-09 12:12:59 +0000

    conversions ki baat

--- 01basic/datatype.js
+++ 01basic/datatype.js
@@ -1,1 +1,8 @@
-console.log("chalja bhaiii")
+let score ="435aa";
+console.log(typeof null);   //conversion sikh rhe hai
+let valueinNumber=Number(score);
+console.log(typeof valueinNumber);
+console.log("value in number is :" + valueinNumber);
+ // "33" convert hojyga in number but "33adc" convert hoga NaN datatype mein....
+ //similiarly we can convert strings/number/values in boolean or with each other...we can convert arrays,class further(Advance)
+ 
